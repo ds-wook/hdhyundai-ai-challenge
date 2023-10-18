ed399bd612cb506fc51479af4c9593ecde1273f0
ds-wook <leewook94@gmail.com>
2023-10-18 15:11:41 +0900

    Update!

--- src/engine/build.py
+++ src/engine/build.py
@@ -12,8 +12,8 @@ class FeatureEngineer(BaseFeatureEngineer):
     def __init__(self, cfg: DictConfig, df: pd.DataFrame):
         super().__init__(cfg)
         df = self._add_time_features(df)
-        # df = self._add_trend_features(df)
-        # df = self._add_basic_features(df)
+        df = self._add_trend_features(df)
+        df = self._add_basic_features(df)
         self.df = df
 
     def get_train_pipeline(self):
@@ -39,21 +39,6 @@ class FeatureEngineer(BaseFeatureEngineer):
         df["hour"] = df["ATA"].dt.hour
         df["minute"] = df["ATA"].dt.minute
         df["weekday"] = df["ATA"].dt.weekday
-        df["sin_time"] = np.sin(2 * np.pi * df.hour / 24)
-        df["cos_time"] = np.cos(2 * np.pi * df.hour / 24)
-
-        return df
-
-    def _fill_missing_features(self, df: pd.DataFrame) -> pd.DataFrame:
-        """
-        Fill missing features
-        Args:
-            df: dataframe
-        Returns:
-            dataframe
-        """
-        for col in tqdm(["AIR_TEMPERATURE", "U_WIND", "V_WIND"], leave=False):
-            df[col] = df[col].fillna(df.groupby("ID")[col].transform("mean"))
 
         return df
 
@@ -65,7 +50,7 @@ class FeatureEngineer(BaseFeatureEngineer):
         Returns:
             dataframe
         """
-        trend_features = ["DUBAI", "BRENT", "AIR_TEMPERATURE", "DIST", "PORT_SIZE"]
+        trend_features = ["AIR_TEMPERATURE", "DIST"]
 
         for col in tqdm(trend_features, leave=False):
             df[f"{col}_diff1"] = df[col] - df.groupby("ARI_PO")[col].shift(1)
@@ -73,17 +58,6 @@ class FeatureEngineer(BaseFeatureEngineer):
         return df
 
     def _add_basic_features(self, df: pd.DataFrame) -> pd.DataFrame:
-        all_cols = [c for c in list(df.columns) if c not in ["ARI_PO", self.cfg.data.target]]
-        num_features = [col for col in all_cols if col not in self.cfg.data.categorical_features]
-        df_num_agg = df.groupby("ARI_PO")[num_features].agg(["mean", "std", "min", "max"])
-        df_num_agg.columns = ["_".join(x) for x in df_num_agg.columns]
-        df_num_agg.reset_index(inplace=True)
-
-        df_cat_agg = df.groupby("ARI_PO")[[*self.cfg.data.categorical_features]].agg(["count", "nunique"])
-        df_cat_agg.columns = ["_".join(x) for x in df_cat_agg.columns]
-        df_cat_agg.reset_index(inplace=True)
-
-        df = df.merge(df_num_agg, how="inner", on="ARI_PO")
-        df = df.merge(df_cat_agg, how="inner", on="ARI_PO")
+        df["DIST"] = np.log1p(df["DIST"])
 
         return df
